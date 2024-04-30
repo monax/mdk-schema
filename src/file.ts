2464af46b98c1addbda5b9373429ab408b682728
Daniel Zahariev <daniel.zahariev@monaxlabs.io>
2024-04-30 19:37:25 +0300

    Update file info fetching

--- src/file.ts
+++ src/file.ts
@@ -1,12 +1,12 @@
-import https from 'https';
-
 export const FileInfo = {
   async getContentType(url: string): Promise<string | undefined> {
     return new Promise((resolve, reject) => {
-      https
-        .request(url, { method: 'HEAD', timeout: 1000 }, (res) => resolve(res.headers['content-type']))
-        .on('error', reject)
-        .end();
+      const ctl = new AbortController();
+      setTimeout(() => ctl.abort('Request Timeout'), 1000);
+
+      fetch(url, { method: 'HEAD', signal: ctl.signal, mode: 'cors' })
+        .then((res) => resolve(res.headers.get('content-type') || undefined))
+        .catch(reject);
     });
   },
 };
