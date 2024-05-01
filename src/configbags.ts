673dbbebfc571642478771068eebda77539ecaa6
Daniel Zahariev <daniel.zahariev@monaxlabs.io>
2024-05-01 10:33:38 +0300

    Move configs from lib

--- src/configbags.ts
+++ src/configbags.ts
@@ -1,11 +1,32 @@
 import * as z from 'zod';
-import { ConfigBag } from './environment.js';
+import { ConfigSpec } from './environment.js';
 
-export type PgConfig = ConfigBag<typeof PgConfigBag>;
-export const PgConfigBag = {
+export type PgConfig = ConfigSpec<typeof PgConfigSpec>;
+export const PgConfigSpec = {
   POSTGRES_DB: z.string(),
   POSTGRES_USER: z.string(),
   POSTGRES_PORT: z.coerce.number().int(),
   POSTGRES_HOST: z.string(),
   POSTGRES_PASSWORD: z.string(),
+} as const;
+
+export type AnalyticsConfig = ConfigSpec<typeof AnalyticsConfigSpec>;
+export const AnalyticsConfigSpec = {
+  SEGMENT_ANALYTICS_API_KEY_BACKEND: z.string().optional(),
+} as const;
+
+export type StorageConfig = ConfigSpec<typeof StorageConfigSpec>;
+export const StorageConfigSpec = {
+  GCP_PROJECT_ID: z.string(),
+  GCP_SERVICE_ACCOUNT: z.string(),
+  GCP_KEY_FILE: z.string().optional(),
+  GCS_HOST: z.string(),
+  GCS_PUBLIC_FILES_BUCKET: z.string(),
+  GCS_PRIVATE_FILES_BUCKET: z.string(),
+} as const;
+
+export type MetricsConfig = ConfigSpec<typeof MetricsConfigSpec>;
+export const MetricsConfigSpec = {
+  METRICS_PORT: z.coerce.number().int().optional(),
+  METRICS_LISTEN_ADDRESS: z.string().optional(),
 } as const;
