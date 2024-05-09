c01872e6eb532e0e0d7a0d17a574238f53531384
Mike Kee <mike.kee@monax.io>
2024-05-09 19:14:03 +0100

    Linting

--- src/string-decimal.ts
+++ src/string-decimal.ts
@@ -1,16 +1,11 @@
-import type * as z from "zod";
-import { Numeric } from "./numeric.js";
-import type {} from "bigdecimal";
+import type * as z from 'zod';
+import { Numeric } from './numeric.js';
+import type {} from 'bigdecimal';
 
 export type StringDecimal = z.infer<typeof StringDecimal>;
-export const StringDecimal = Numeric.transform((v) => v.toString()).brand(
-  "StringDecimal",
-);
+export const StringDecimal = Numeric.transform((v) => v.toString()).brand('StringDecimal');
 
 export type PositiveStringDecimal = z.infer<typeof PositiveStringDecimal>;
-export const PositiveStringDecimal = Numeric.refine(
-  (v) => v.gte(0),
-  "invalid_positive_decimal_string",
-)
+export const PositiveStringDecimal = Numeric.refine((v) => v.gte(0), 'invalid_positive_decimal_string')
   .transform((v) => v.toString())
-  .brand("PositiveStringDecimal");
+  .brand('PositiveStringDecimal');
