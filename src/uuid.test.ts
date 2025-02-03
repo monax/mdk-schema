1787c370cb10142f3b81facdf88a72bab89fca47
Daniel Zahariev <daniel.zahariev@monaxlabs.io>
2025-02-03 11:03:23 +0200

    Upgrade dependencies and updates

--- src/uuid.test.ts
+++ src/uuid.test.ts
@@ -1,10 +1,10 @@
-import { v4 } from 'uuid';
+import { uuidv4 } from 'uuidv7';
 import { describe, expect, test } from 'vitest';
 import { UUID, createUUID } from './uuid.js';
 
 describe('UUID', () => {
   test('Parsing', () => {
-    UUID.parse(v4());
+    UUID.parse(uuidv4());
     const uuid = createUUID();
     expect(() => UUID.parse(uuid.slice(0, 35))).toThrow('invalid_string');
   });
